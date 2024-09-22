c534db07ebab82b2f56fa248e930ee959e0020f3
Sanjar <sanjarrr28@gmail.com>
2024-09-22 17:15:20 +0200

    tests gouped by 'describe'

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,14 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  it 'belongs to a question' do
-    answer = create(:answer)
-    question = answer.question
-    expect(answer.question).to eq(question)
+  describe 'associations' do
+    it 'belongs to a question' do
+      answer = create(:answer)
+      question = answer.question
+      expect(answer.question).to eq(question)
+    end
   end
 
-  it 'validates presence of body' do
-    answer = Answer.new(body: nil)
-    expect(answer).to be_invalid
+  describe 'validations' do
+    it 'validates presence of body' do
+      answer = Answer.new(body: nil)
+      expect(answer).to be_invalid
+    end
   end
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,22 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  it 'has many answers' do
-    question = create(:question)
-    create(:answer, question: question)
-    create(:answer, question: question)
-
-    expect(question.answers.count).to eq(2)
+  describe 'associations' do
+    it 'has many answers' do
+      question = create(:question)
+      create(:answer, question: question)
+      create(:answer, question: question)
+      expect(question.answers.count).to eq(2)
+    end
   end
 
-  it 'validates presence of title' do
-    question = Question.new(body: '123')
-    expect(question).to be_invalid
-  end
+  describe 'validations' do
+    it 'validates presence of title' do
+      question = Question.new(body: '123')
+      expect(question).to be_invalid
+    end
 
-  it 'validates presence of body' do
-    question = Question.new(title: '123')
-    expect(question).to be_invalid
+    it 'validates presence of body' do
+      question = Question.new(title: '123')
+      expect(question).to be_invalid
+    end
   end
 
 end
