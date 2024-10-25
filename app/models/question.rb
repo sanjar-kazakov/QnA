efded5ef55317d96996a3f08038da3ac7551bbe9
Sanjar <sanjarrr28@gmail.com>
2024-10-25 22:49:48 +0200

    files attachment

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,6 +3,8 @@ class Question < ApplicationRecord
   belongs_to :user
   belongs_to :best_answer, class_name: 'Answer', optional: true
 
+  has_many_attached :files
+
   validates :title, presence: true
   validates :body, presence: true
 
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -70,4 +70,8 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.after(:all) do
+    FileUtils.rm_rf("#{Rails.root}/tmp/storage")
+  end
 end
